d18467eca03957d6005acd102ab93f0876f2667c
Ameen Y <113547608+ameeny@users.noreply.github.com>
2023-08-08 23:49:01 -0600

    Add slack signing verification

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
+from flask import Flask, request, abort
 import logging
-import ssl
+from functools import wraps
+import time
+import sys
 from functions import draft_email, draft_slack,reply_robot
 # ssl._create_default_https_context = ssl._create_unverified_context
 # logging.basicConfig(level=logging.DEBUG)
 
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # Set Slack API credentials
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
@@ -28,6 +31,35 @@ flask_app = Flask(__name__)
 flask_app.config['WTF_CSRF_ENABLED'] = False
 handler = SlackRequestHandler(app)
 
+
+signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
+
+def require_slack_verification(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not verify_slack_request():
+            abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def verify_slack_request():
+    # Get the request headers
+    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+    signature = request.headers.get("X-Slack-Signature", "")
+
+    # Check if the timestamp is within five minutes of the current time
+    current_timestamp = int(time.time())
+    if abs(current_timestamp - int(timestamp)) > 60 * 5:
+        return False
+
+    # Verify the request signature
+    return signature_verifier.is_valid(
+        body=request.get_data().decode("utf-8"),
+        timestamp=timestamp,
+        signature=signature,
+    )
 
 def get_bot_user_id():
     """
@@ -122,6 +154,7 @@ def reply_slack(ack, say, command):
 
 
 @flask_app.route("/slack/events", methods=["POST"])
+@require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
@@ -141,4 +174,5 @@ def slack_events():
 
 # Run the Flask app
 if __name__ == "__main__":
-    flask_app.run(port=5002)
+    logging.info('Flask app starting')
+    flask_app.run(host="0.0.0.0", port=8000)
